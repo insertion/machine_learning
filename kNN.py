de158e99c6e9ec0f5ca7a9db3f404358c052b2e1
mint <398758695@qq.com>
2016-01-27 19:08:23 +0800

    classify0 finished

--- kNN.py
+++ kNN.py
@@ -2,13 +2,31 @@
 import numpy,operator
 def createDataset():
 	group=numpy.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-	labels=['A','A','B','B']
+	labels=['B','B','A','A']
 	return group,labels
 def clssify0(inx,dataset,labels,k):
 	datasize=dataset.shape[0]
-	diffmat=tile(inx,(datasize,1))-dataset
-	sqdiffmat=diffmat*2
+	#print(datasize)
+	diffmat=numpy.tile(inx,(datasize,1))-dataset
+	print(diffmat,'\n')
+	sqdiffmat=diffmat**2
+	print(sqdiffmat,'\n')
 	sqdistances=sqdiffmat.sum(axis=1)
+	#clom is axis==0,row is axis=1
+	print(sqdistances)
 	distances=sqdistances**0.5
-	
+	sortddistnces=distances.argsort()
+	classCount={}
+	for i in range(k):
+		vote=labels[sortddistnces[i]]
+		classCount[vote]=classCount.get(vote,0)+1
+	sortclasscount=sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+	#sort classcount'interitems
+	print(sortclasscount)
+	return sortclasscount[0][0]
 	pass
+
+if __name__ == '__main__':
+	a,b=createDataset()
+	c=clssify0([1,0.5],a,b,3)
+	print(c)
