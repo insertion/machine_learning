295d7ace0661f8b0a0f4f64701206e6189271d3e
mint <398758695@qq.com>
2016-01-27 22:09:18 +0800

    modified:   kNN.py

--- kNN.py
+++ kNN.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
-import numpy,operator
+import numpy
+import matplotlib
+import matplotlib.pyplot as plt
+import operator
+def file2matrix(filename):
+	#infiles everyline have four element ,last one is leble
+	files=open(filename)
+	lines=files.readlines()
+	numberofline=len(lines)
+	returnMat=numpy.zeros((numberofline,3))
+	# rows and cloms
+	classlabvector=[]
+	index=0
+	for line in lines:
+		line=line.strip()
+		#removed whitespace
+		listformline=line.split('\t')
+		# split line by '\t'
+		returnMat[index:]=listformline[0:3]
+		#write data into everyrows of matrix
+		classlabvector.append(listformline[-1])
+		#list[-1] to get the last element
+		index+=1
+	return returnMat,classlabvector
+	pass
 def createDataset():
 	group=numpy.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
 	labels=['B','B','A','A']
@@ -30,3 +54,10 @@ if __name__ == '__main__':
 	a,b=createDataset()
 	c=clssify0([1,0.5],a,b,3)
 	print(c)
+	datamat,datalab=file2matrix("datingTestSet.txt")
+	print(datamat[:,0])
+	fig=plt.figure()
+	ax=fig.add_subplot(111)
+	ax.scatter(datamat[:,1],datamat[:,2])
+	plt.show()
+	#print(datalab)
